07a1750d57bb316c9adc5936b5a492d5d64f3757
Ali Fadda <69652815+silvertechguy@users.noreply.github.com>
2020-10-12 21:18:53 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,17 @@ const main = async () => {
   const app: Express = express();
   app.use(cors());
   const redis = new Redis(process.env.REDIS_URL);
+   
+   app.get(
+    "/",
+    async (req: Request, res: Response): Promise<Response> => {
+      try {
+        return res.json({ msg: "You are good to go using this amazing API" });
+      } catch (error) {
+        return res.json({ error });
+      }
+    }
+  
   app.get(
     "/api/v1/ppass",
     async (req: Request, res: Response): Promise<Response> => {
